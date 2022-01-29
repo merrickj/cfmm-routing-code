d0e5bc5d374920ed4d3b5730e4ae2cd2f1f6d32f
merrickj <jamesmerrick@hotmail.com>
2022-01-29 22:29:54 +0000

    preparing for MOS

--- arbitrage.py
+++ arbitrage.py
@@ -1,8 +1,12 @@
+#@ Model: Constant Function Market Makers
+
 import numpy as np
 import cvxpy as cp
 
 # Problem data
+#@ Helper Object: global_indices
 global_indices = list(range(4))
+#@ Helper Object: local_indices
 local_indices = [
     [0, 1, 2, 3],
     [0, 1],
@@ -10,7 +14,7 @@ local_indices = [
     [2, 3],
     [2, 3]
 ]
-
+#@ Helper Object: reserves
 reserves = list(map(np.array, [
     [4, 4, 4, 4],
     [10, 1],
@@ -18,7 +22,7 @@ reserves = list(map(np.array, [
     [40, 50],
     [10, 10]
 ]))
-
+#@ Helper Object: fees
 fees = [
     .998,
     .997,
@@ -26,7 +30,7 @@ fees = [
     .997,
     .999
 ]
-
+#@ Helper Object: market_value
 # "Market value" of tokens (say, in a centralized exchange)
 market_value = [
     1.5,
@@ -36,9 +40,12 @@ market_value = [
 ] 
 
 # Build local-global matrices
+#@ Helper Object: n
 n = len(global_indices)
+#@ Helper Object: m
 m = len(local_indices)
 
+#@ Helper Object: A
 A = []
 for l in local_indices:
     n_i = len(l)
@@ -48,37 +55,63 @@ for l in local_indices:
     A.append(A_i)
 
 # Build variables
+#@ Variable: deltas
 deltas = [cp.Variable(len(l), nonneg=True) for l in local_indices]
+#@ Variable: lambdas
 lambdas = [cp.Variable(len(l), nonneg=True) for l in local_indices]
 
+#@ Function: psi
 psi = cp.sum([A_i @ (L - D) for A_i, D, L in zip(A, deltas, lambdas)])
 
 # Objective is to maximize "total market value" of coins out
 obj = cp.Maximize(market_value @ psi)
 
 # Reserves after trade
+#@ Function: new_reserves
 new_reserves = [R + gamma_i*D - L for R, gamma_i, D, L in zip(reserves, fees, deltas, lambdas)]
+
+#@ Constraint: balancer_pool
+#@ Description: Balancer pool with weights 4, 3, 2, 1
+balancer_pool = cp.geo_mean(new_reserves[0], p=np.array([4, 3, 2, 1])) >= cp.geo_mean(reserves[0])
+
+#@ Constraint: uniswap1
+#@ Description: Uniswap v2 pools
+uniswap1 = cp.geo_mean(new_reserves[1]) >= cp.geo_mean(reserves[1])
+
+#@ Constraint: uniswap2
+#@ Description: Uniswap v2 pools
+uniswap2 = cp.geo_mean(new_reserves[2]) >= cp.geo_mean(reserves[2])
+
+#@ Constraint: uniswap3
+#@ Description: Uniswap v2 pools
+uniswap3 = cp.geo_mean(new_reserves[3]) >= cp.geo_mean(reserves[3])
+
+#@ Constraint: constant_sum_pool1
+#@ Description: Constant sum pool
+constant_sum_pool1 = cp.sum(new_reserves[4]) >= cp.sum(reserves[4])
+
+#@ Constraint: constant_sum_pool2
+#@ Description: Constant sum pool
+constant_sum_pool2 = new_reserves[4] >= 0
+
+#@ Constraint: arbitrage
+#@ Description: Arbitrage constraint
+arbitrage = psi >= 0
+
 
 # Trading function constraints
 cons = [
-    # Balancer pool with weights 4, 3, 2, 1
-    cp.geo_mean(new_reserves[0], p=np.array([4, 3, 2, 1])) >= cp.geo_mean(reserves[0]),
-
-    # Uniswap v2 pools
-    cp.geo_mean(new_reserves[1]) >= cp.geo_mean(reserves[1]),
-    cp.geo_mean(new_reserves[2]) >= cp.geo_mean(reserves[2]),
-    cp.geo_mean(new_reserves[3]) >= cp.geo_mean(reserves[3]),
-
-    # Constant sum pool
-    cp.sum(new_reserves[4]) >= cp.sum(reserves[4]),
-    new_reserves[4] >= 0,
-
-    # Arbitrage constraint
-    psi >= 0
-]
+    balancer_pool,
+    uniswap1,
+    uniswap2,
+    uniswap3,
+    constant_sum_pool1,
+    constant_sum_pool2,
+    arbitrage]
 
 # Set up and solve problem
+#@ Problem: prob
 prob = cp.Problem(obj, cons)
-prob.solve()
+prob.solve(verbose=True)
 
 print(f"Total output value: {prob.value}")
